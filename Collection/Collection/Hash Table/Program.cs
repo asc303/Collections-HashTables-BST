08202158f2e6764c3e5bbfb767f936a5316c9402
asc303 <chauhansam94@gmail.com>
2021-11-26 17:33:42 +0530

    [ashutosh] add:Hash Table

---
+++ Collection/Collection/Hash Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table ");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
+            hash.Add("1", "On");
+            hash.Add("2", "Way");
+            hash.Add("3", "or");
+            hash.Add("4", "not");
+            hash.Add("5", "On");
+            hash.Add("6", "Way");
+            string hash6 = hash.Get("6");
+            Console.WriteLine("6th Index Value: " + hash6);
+        }
+    }
+}
